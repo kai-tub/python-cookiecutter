d29cbfb7518ec302ab98916b25584939d4e826f6
Kai Norman Clasen <kai.ce.tub@gmail.com>
2022-10-15 16:29:27 +0200

    Move source code to src directory
    It is frequently recommended but I never felt a strong reason to use a separate src directory.
    After working with a couple of projects, the 'best' reason for me is to allow my muscle-memory to 'remember' where the source directory is within the root directory.
    
    Due to update to pdm-bump, I do not need to convert the bump type anymore.
    
    Updated the pre-commit files.
    Removed the default version constraints from dev dependencies

--- {{cookiecutter.__project_slug}}/.auto/update_version.sh
+++ {{cookiecutter.__project_slug}}/.auto/update_version.sh
@@ -23,10 +23,6 @@ if [[ -z "$(command -v pdm)" ]]; then
     error "requires PDM to bump version!"
 fi
 
-# Until https://github.com/carstencodes/pdm-bump/pull/2 is merged
-if [[ "$bumpType" = "patch" ]]; then
-    bumpType="micro"
-fi
 # bump version
 pdm bump "$bumpType"
 
